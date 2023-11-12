e394f2c02afd18fa8059e825e87a2a8d9b7cb7b3
alif-dot <alif17648@outlook.com>
2023-11-12 15:36:31 +0600

    Add project files.

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,31 @@
+﻿using MailKit.Security;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MimeKit;
+using MimeKit.Text;
+using System.Net.Mail;
+using MailKit.Net.Smtp;
+using SendEmailWebApi.Models;
+using SendEmailWebApi.Services;
+
+namespace SendEmailWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmailController : ControllerBase
+    {
+        private readonly IEmailService _emailService;
+
+        public EmailController(IEmailService emailService)
+        {
+            _emailService = emailService;
+        }
+
+        [HttpPost]
+        public IActionResult SendEmail(EmailDto request)
+        {
+            _emailService.SendEmail(request);
+            return Ok();
+        }
+    }
+}
---
+++ Services/IEmailService.cs
@@ -0,0 +1,9 @@
+﻿using SendEmailWebApi.Models;
+
+namespace SendEmailWebApi.Services
+{
+    public interface IEmailService
+    {
+        void SendEmail(EmailDto request);
+    }
+}
